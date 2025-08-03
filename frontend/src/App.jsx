12f14fc04f6f921c7d7a46304b566280b564a781
Bbitq <bbitq2233@gmail.com>
2025-08-03 23:24:22 +0800

    frontend-2025-8-3.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  NavLink,
+} from "react-router-dom";
+// layout
+function LayOut() {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/logs" end>
+          LogHome
+        </NavLink>
+        <NavLink to="/logs/all" end>
+          LogAll
+        </NavLink>
+        <NavLink to="/logs/since" end>
+          LogSince
+        </NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+// logs
+function LogHome() {
+  return (
+    <div>
+      <h2>LogHome</h2>
+    </div>
+  );
+}
+// logs/all
+function LogAll() {
+  return (
+    <main>
+      <header>
+        <h2>LogAll</h2>
+      </header>
+      <section>
+        <h3>这里展示日志</h3>
+      </section>
+      <footer>
+        <p>正在搭建ing...</p>
+      </footer>
+    </main>
+  );
+}
+// logs/since
+function LogSince() {
+  return (
+    <div>
+      <h2>LogSince</h2>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LayOut />}>
+            <Route path="logs" element={<LogHome />} />
+            <Route path="/logs/all" element={<LogAll />} />
+            <Route path="/logs/since" element={<LogSince />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
